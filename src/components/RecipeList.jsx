6e2fa70e256d54b530f47b81904a87ab42e1db03
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-13 11:38:21 -0500

    add recipe list and card css

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -28,4 +28,8 @@ const RecipeList = ({ meals }) => {
 
 export default RecipeList;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+`;
