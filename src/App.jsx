c41c7d41abf5d3ffde88d39b160cf7b719c08f29
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-14 15:49:42 -0500

    add react query

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import {
 import { loader as landingLoader } from './pages/Landing';
 import { loader as singleRecipeLoader } from './pages/Recipe';
 import { action as newsLetterAction } from './pages/NewsLetter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const router = createBrowserRouter([
   {
@@ -43,7 +45,21 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
+
 export default App;
