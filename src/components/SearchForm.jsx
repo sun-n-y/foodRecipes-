2114f8fc5f219b42d5ed970c6663cd562ab6fe14
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-14 15:32:15 -0500

    add search css

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -23,4 +23,20 @@ const SearchForm = ({ searchTerm }) => {
 };
 export default SearchForm;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-bottom: 6rem;
+  .form {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+
+  .form-input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .btn {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`;
